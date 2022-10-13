a3bfa4480b986f795ef5f74757a5287550d95878
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2022-10-13 18:09:24 +0200

    update file structure and extends style

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './style.scss'
+
+// Menu-animation
+
+var menuVisibility = false
+$(document).ready(function() {
+
+})
+
+$(document).ready(function() {
+    console.log("document ready")
+        // $(body).scroll(function() {
+        //     console.log("scroll")
+        // });
+});
